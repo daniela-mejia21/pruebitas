eca9c4fd23b8ef559164abf76849b5a0f605b462
Daniela Mejia <dmejia@cherrytech.com.mx>
2026-08-17 19:03:10 -0600

    Primer commit

---
+++ HolaMundo/HolaMundo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HolaMundo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Holi jefecito!");
+        }
+    }
+}
---
+++ SumaDeNumerosEnteros/SumaDeNumerosEnteros/Program.cs
@@ -0,0 +1,13 @@
+﻿// Pedir el primer número
+Console.Write("Ingresa el primer número: ");
+int num1 = int.Parse(Console.ReadLine());
+
+// Pedir el segundo número
+Console.Write("Ingresa el segundo número: ");
+int num2 = int.Parse(Console.ReadLine());
+
+// Sumar los números
+int suma = num1 + num2;
+
+// Mostrar el resultado
+Console.WriteLine("La suma es: " + suma);
